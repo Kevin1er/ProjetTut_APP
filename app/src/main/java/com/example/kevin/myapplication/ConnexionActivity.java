585912c871ad34ff11e2379820b5c572995fa2cd
DanRobS <roberttsoumbou@gmail.com>
2018-03-18 16:58:54 +0100

    Creation interfaces "Connexion" et "Inscription" à l'appli

--- app/src/main/java/com/example/kevin/myapplication/ConnexionActivity.java
+++ app/src/main/java/com/example/kevin/myapplication/ConnexionActivity.java
@@ -1,7 +1,10 @@
 package com.example.kevin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConnexionActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ConnexionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        Button connexion = (Button)findViewById(R.id.button_connexion);
+        Button new_compte = (Button)findViewById(R.id.creer_compte);
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        new_compte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
     }
+
 }
